9c8f0a710d9256ba3936a3ce74ae650263293cf6
Orange Marques dos Santos <orange.santos@itriad.org.br>
2019-02-13 09:44:00 -0400

    added first version of the project

---
+++ SocketServerLaserMock/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SocketServerLaserMock
+{
+    class Program
+    {
+        private static NetworkStream socketStream;
+        private static TcpListener servidor = new TcpListener(50002);
+        private static BinaryWriter escreve;
+        private static BinaryReader ler;
+        private static Thread threadSocket;
+
+        static void Main(string[] args)
+        {
+            threadSocket = new Thread(new ThreadStart(RunSocketServer));
+            threadSocket.Start();
+        }
+
+        public static void RunSocketServer()
+        {
+            servidor.Start();
+
+            while(true)
+            {
+                Socket conexao = servidor.AcceptSocket();
+                socketStream = new NetworkStream(conexao);
+                escreve = new BinaryWriter(socketStream);
+                ler = new BinaryReader(socketStream);
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while (true)
+                {
+                    try
+                    {
+                        byte[] message = ler.ReadBytes(1);
+                        Console.Write($"{System.Text.Encoding.Default.GetString(message)}");
+                        stringBuilder.Append(System.Text.Encoding.Default.GetString(message));
+
+                        if (BitConverter.ToString(message) == "0A")
+                        {
+                            string msg = stringBuilder.ToString();
+                            stringBuilder.Clear();
+                            Console.Write($"msg = {msg}");
+
+                            if(msg.StartsWith("WX,ProgramNo="))
+                            {
+                                System.Threading.Thread.Sleep(500);
+                                byte[] bytes = Encoding.ASCII.GetBytes("WX,OK\r");
+                                escreve.Write(bytes);
+                            }
+                            else if(msg.StartsWith("WX,PRG="))
+                            {
+                                System.Threading.Thread.Sleep(500);
+                                byte[] bytes = Encoding.ASCII.GetBytes("WX,OK\r");
+                                escreve.Write(bytes);
+                            }
+                            else if (msg.StartsWith("WX,StartMarking"))
+                            {
+                                System.Threading.Thread.Sleep(3000);
+                                byte[] bytes = Encoding.ASCII.GetBytes("WX,OK\r");
+                                escreve.Write(bytes);
+                            }
+                            else if (msg.StartsWith("WX,ErrorClear"))
+                            {
+                                System.Threading.Thread.Sleep(1000);
+                                byte[] bytes = Encoding.ASCII.GetBytes("WX,OK\r");
+                                escreve.Write(bytes);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"ex.Message = {ex.Message}");
+                        //break;
+                    }
+                    //System.Threading.Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
---
+++ SocketServerLaserMock/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("SocketServerLaserMock")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SocketServerLaserMock")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("7fc7c690-3544-4ba8-ac4a-1a2b2e1a3523")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
